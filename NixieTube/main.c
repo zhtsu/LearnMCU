0904d02ea02a86410342c15c2511ede06dcb138d
zhtsu <zhtsu@qq.com>
2024-05-13 19:44:06 +0800

    Update NixieTube

--- NixieTube/main.c
+++ NixieTube/main.c
@@ -1,11 +1,34 @@
 #include <REGX52.H>
 
+typedef struct
+{
+	int A;
+	int B;
+	int C;
+} F74LS138;
+
+F74LS138 TubeTable[] = {
+	{1, 1, 1}, {1, 1, 0}, {1, 0, 1}, {1, 0, 0},
+	{0, 1, 1}, {0, 1, 0}, {0, 0, 1}, {0, 0, 0}
+};
+
+unsigned char DigitTable[] = {
+	0x3F, 0x06, 0x5B, 0x4F,
+	0x66, 0x6D, 0x7D, 0x7F, 0x6F
+};
+
+void DisplayDigit(unsigned char Location, Number)
+{
+	P2_4 = TubeTable[Location].A;
+	P2_3 = TubeTable[Location].B;
+	P2_2 = TubeTable[Location].C;
+	
+	P0 = DigitTable[Number];
+}
+
 void main()
 {
-	P2_1 = 1;
-	P2_3 = 0;
-	P2_2 = 0;
-	P0 = 0x7D;
+	DisplayDigit(7, 2);
 	
 	while (1)
 	{
