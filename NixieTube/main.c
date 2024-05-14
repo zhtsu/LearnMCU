80ffbb16401c70ece8110127c7b6a828afcedc47
zhtsu <zhtsu@qq.com>
2024-05-14 22:12:23 +0800

    Update project NixieTube

--- NixieTube/main.c
+++ NixieTube/main.c
@@ -17,6 +17,20 @@ unsigned char DigitTable[] = {
 	0x66, 0x6D, 0x7D, 0x7F, 0x6F
 };
 
+// Delay for @11.0592MHz
+void DelayMs(unsigned int xms)
+{
+	unsigned char i, j;
+	
+	while (xms--)
+	{
+		for (i = 2; i > 0; i--)
+		{
+			for (j = 199; j > 0; j--);
+		}
+	}
+}
+
 void DisplayDigit(unsigned char Location, Number)
 {
 	P2_4 = TubeTable[Location].A;
@@ -24,14 +38,20 @@ void DisplayDigit(unsigned char Location, Number)
 	P2_2 = TubeTable[Location].C;
 	
 	P0 = DigitTable[Number];
+	
+	DelayMs(1);
+	P0 = 0x00;
 }
 
 void main()
 {
-	DisplayDigit(7, 2);
-	
 	while (1)
 	{
-		
+		DisplayDigit(0, 1);
+		DisplayDigit(1, 1);
+		DisplayDigit(2, 4);
+		DisplayDigit(4, 5);
+		DisplayDigit(5, 1);
+		DisplayDigit(6, 4);
 	}
 }
