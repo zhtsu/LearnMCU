b42da4f70dced155efb863ba78c1da4545530a89
zhtsu <zhtsu@qq.com>
2024-05-19 19:18:40 +0800

    Update project MatrixKeyboard

--- MatrixKeyboard/main.c
+++ MatrixKeyboard/main.c
@@ -42,17 +42,45 @@ unsigned char MatrixKey()
 	return KeyNum;
 }
 
+unsigned char KeyNum = 0;
+unsigned int Password = 0;
+unsigned char Count = 0;
+
+void ResetPassword()
+{
+	Password = 0;
+	Count = 0;
+	LCD_ShowNum(2, 1, Password, 4);
+}
+
 void main()
 {
-	unsigned char KeyNum;
-	unsigned int Password = 0;
-	unsigned char Count = 0;
-	
 	LCD_Init();
 	LCD_ShowString(1, 1, "Password:");
 	while(1)
 	{
 		KeyNum = MatrixKey();
+		
+		if (KeyNum == 0) continue;
+		
+		if (KeyNum == 11)
+		{
+			if (Password == 1234)
+			{
+				LCD_ShowString(1, 14, "OK ");
+				ResetPassword();
+			}
+			else
+			{
+				LCD_ShowString(1, 14, "ERR");
+				ResetPassword();
+			}
+		}
+		else if (KeyNum == 12)
+		{
+			ResetPassword();
+			LCD_ShowString(1, 14, "   ");
+		}
 		
 		if (KeyNum == 0 || KeyNum > 10 || Count == 4)
 		{
